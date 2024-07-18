29f610ff69b4fcfb45a919ad53d546a12b2ba5da
kellyDMmendes <152336851+kellyDMmendes@users.noreply.github.com>
2024-07-17 21:23:10 -0300

    Create 1004.c

---
+++ 1004.c
@@ -0,0 +1,13 @@
+/* 1004 - Produto Simples */
+#include <stdio.h>
+ 
+int main() {
+ 
+    int a, b, prod;
+    scanf("%d", &a);
+    scanf("%d", &b);
+    prod = a * b;
+    printf("PROD = %d\n", prod);
+ 
+    return 0;
+}
