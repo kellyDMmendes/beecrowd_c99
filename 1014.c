40ed9abda17f12fb5828b9c0ac1c58ba335e7f8f
kellyDMmendes <152336851+kellyDMmendes@users.noreply.github.com>
2024-07-17 21:33:30 -0300

    Create 1014.c

---
+++ 1014.c
@@ -0,0 +1,17 @@
+/* 1014 - Consumo */
+#include <stdio.h>
+ 
+int main() {
+ 
+    int x;
+    double y, consumo;
+    
+    scanf("%d", &x);
+    scanf("%lf", &y);
+    
+    consumo = x/y;
+    
+    printf("%.3lf km/l\n", consumo);
+ 
+    return 0;
+}
