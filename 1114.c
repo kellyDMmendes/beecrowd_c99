1b93bb4a8ad54d574f6c7ea97ac4e01f8f493c94
kellyDMmendes <152336851+kellyDMmendes@users.noreply.github.com>
2024-07-17 22:23:56 -0300

    Create 1114.c

---
+++ 1114.c
@@ -0,0 +1,18 @@
+/* 1114 - Senha Fixa */
+#include <stdio.h>
+ 
+int main() {
+ 
+    int senha;
+    while(1){
+        scanf("%d", &senha);
+        if(senha==2002){
+            printf("Acesso Permitido\n");
+            break;
+        }else{
+            printf("Senha Invalida\n");
+        }
+    }
+ 
+    return 0;
+}
