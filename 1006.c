bbf2c7200c34cf936bb887ec3a7345859b98282e
kellyDMmendes <152336851+kellyDMmendes@users.noreply.github.com>
2024-07-17 21:25:12 -0300

    Create 1006.c

---
+++ 1006.c
@@ -0,0 +1,14 @@
+/* 1006 - Média 2 */
+#include <stdio.h>
+ 
+int main() {
+ 
+    double a, b, c, media;
+    scanf("%lf", &a);
+    scanf("%lf", &b);
+    scanf("%lf", &c);
+    media = ((2*a)+(3*b)+(5*c))/10;
+    printf("MEDIA = %.1lf\n", media);
+ 
+    return 0;
+}
