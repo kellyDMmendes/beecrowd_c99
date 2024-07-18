3a7fa34706b3bc4a8e8641b000222f55c29a50d9
kellyDMmendes <152336851+kellyDMmendes@users.noreply.github.com>
2024-07-17 21:55:53 -0300

    Update 1046.c

--- 1046.c
+++ 1046.c
@@ -7,7 +7,7 @@ int main() {
     
     scanf("%d %d", &i, &f);
     
-    if(i>=0 && f>=0 && i<24 && f<24){
+    
         if(i==f){
             h = 24;
         }else if(i<f){
@@ -16,7 +16,7 @@ int main() {
             h = (24-i)+f;
         }
         printf("O JOGO DUROU %d HORA(S)\n", h);
-    }
+    
  
     return 0;
 }
