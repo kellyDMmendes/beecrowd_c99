dbabaf7ee0256ccc2dd9be65f36277021d0c719a
kellyDMmendes <152336851+kellyDMmendes@users.noreply.github.com>
2024-07-17 21:34:58 -0300

    Create 1016.c

---
+++ 1016.c
@@ -0,0 +1,15 @@
+/* 1016 - Distância */
+#include <stdio.h>
+ 
+int main() {
+ 
+    int x, distancia;
+    
+    scanf("%d", &x);
+    
+    distancia = x*2;
+    
+    printf("%d minutos\n", distancia);
+ 
+    return 0;
+}
