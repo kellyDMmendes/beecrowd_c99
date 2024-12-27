244805cd29461d020faf3368b692f1108e96f2a8
Kelly <kellymendes169@gmail.com>
2024-12-27 18:17:20 -0300

    1182 - Coluna na Matriz

---
+++ teste.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(){
+	int c, i, j;
+	float m[12][12], soma = 0;
+	char t;
+	
+	scanf("%d", &c);
+	scanf(" %c", &t);
+    for(i = 0; i < 12; i++){
+        for(j = 0; j < 12; j++){
+            scanf("%f", &m[i][j]);
+            if(j == c){
+                soma += m[i][j];
+            }
+        }
+    }
+    if(t == 'M'){
+        soma /= 12;
+    }
+    printf("%.1f\n", soma);
+    
+    return 0;
+}
